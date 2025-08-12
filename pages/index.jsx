2493ae209f9d94fb620e121543b03f4afa319eff
Nicolas Wester <nwester627@yahoo.com>
2025-08-12 09:02:17 -0400

    WIP Google Tag implementation

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import { React } from "react";
+import React from "react";
 import ContactForm from "@/components/Contact";
 import Testimonials from "@/components/Testimonials";
 import Projects from "@/components/Projects";
@@ -8,10 +8,23 @@ import Intro from "@/components/Intro";
 import FrostedSection from "@/components/FrostedSection";
 
 export default function Home() {
+  const GATag = `
+  window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-YF4XMJ3SB2');`;
+
   return (
     <div>
       <Head>
-        <title>Nicolas Wester's Portfolio</title>
+        <title>Nicolas Wester Portfolio</title>
+
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-YF4XMJ3SB2"
+        ></script>
+
+        <script dangerouslySetInnerHTML={{ __html: GATag }} />
       </Head>
 
       <main className="bg-gradient-to-br from-space to to-black text-light-gray font-poppins">
