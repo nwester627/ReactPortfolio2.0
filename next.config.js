b36c7d7ac16be5c65d53c10aac465f0332d7948e
Nicolas Wester <nwester627@yahoo.com>
2025-08-12 09:45:34 -0400

    Config update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   distDir: "dist",
+  basePath: "/ReactPortfolio2.0",
+  assetPrefix: "/ReactPortfolio2.0",
   images: {
     unoptimized: true,
   },
